bf22cb3d3a72fffc7c7696b02c248aa5fa299675
Luis Martínez Redondo <lmartinezr10@educarex.es>
2025-09-24 10:19:36 +0200

    Ejemplo variables, tipos de datos e identificadores

---
+++ src/unidad1/ejemplos/Programa.java
@@ -0,0 +1,58 @@
+package unidad1.ejemplos;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+       //TIPOS DE DATOS
+		
+	   // Numéricos
+		//Enteros
+			//Declaración + Inicialización (Asignación)
+			byte edadAlumnoCicloDam = 24;
+			//Asignación
+			edadAlumnoCicloDam = 25;
+			
+			short anioNacimiento;
+			anioNacimiento=2000;
+			int personas;
+			personas = 26000;
+			long particulas;
+			particulas = 93939393;
+			
+		//Como flotante
+			float nota;
+			nota = 6.7f;
+			double saldo;
+			saldo=164000.678;
+		
+	   // Caracteres
+			char letra;
+			letra='T';
+			String codigo;
+			codigo= "KDIS234";
+		
+	   // Booleanos-lógicos
+			boolean carneConducir; // almacena un true o false
+			carneConducir = true;
+			boolean tieneCoche;
+			tieneCoche = false;
+			
+			
+		
+			System.out.println("Edad:"+edadAlumnoCicloDam+" años");
+			System.out.println("Año nacimiento:"+anioNacimiento);
+			
+		
+		
+	   
+		/*
+	    * Esto es un comentario de varias líneas
+	    * Lo puedo aprovechar para añadir indicaciones o aclaraciones en mi código
+	    */
+		
+		
+		
+	}
+
+}
