deedfac1a5137463ee8534d04637b362082996a9
Luis Martínez Redondo <lmartinezr10@educarex.es>
2025-09-29 13:58:33 +0200

    Ejmplo lectura de datos por argumentos

---
+++ src/unidad1/ejemplos/EjemploLecturaDatos.java
@@ -0,0 +1,15 @@
+package unidad1.ejemplos;
+
+public class EjemploLecturaDatos {
+
+	public static void main(String[] args) {
+		
+		
+		String nombre=args[0];
+		String ciclo=args[1];
+		
+		System.out.println("Hola "+nombre+" estás matriculado en "+ciclo+".Bienvenid@");
+
+	}
+
+}
--- src/unidad1/ejemplos/Inicio.java
+++ src/unidad1/ejemplos/Inicio.java
@@ -4,7 +4,7 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		
-		System.out.println("1DAM-25-26");
+		System.out.println("Hola, estamos a lunes");
 		
 	}
 
