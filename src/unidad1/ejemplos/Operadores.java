1d84a4a340d1c0dad40fe0acbd941cfce241452c
Luis Martínez Redondo <lmartinezr10@educarex.es>
2025-09-29 14:34:58 +0200

    Ejemplo operadores

---
+++ src/unidad1/ejemplos/Operadores.java
@@ -0,0 +1,26 @@
+package unidad1.ejemplos;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		//Operadores aritméticos
+		//+,-,*,/,%
+		int num1 = 10, num2=20;
+		
+		int resultadoSuma = num1 + num2;
+		
+		System.out.println("Suma:"+resultadoSuma);
+		
+		System.out.println("Suma:"+(num1+num2));
+		
+		int resultadoResta = num1 - num2;
+		
+		System.out.println("Suma:"+resultadoResta);
+		
+		
+		
+
+	}
+
+}
