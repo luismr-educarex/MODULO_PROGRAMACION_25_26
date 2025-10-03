eb28d1c4917028f5f518e525a0d03b587596c300
Luis Martínez Redondo <lmartinezr10@educarex.es>
2025-10-03 09:36:47 +0200

    Ejercicio nombre y tipos de variables

---
+++ src/unidad1/ejercicios/CrearVariables.java
@@ -0,0 +1,32 @@
+package unidad1.ejercicios;
+
+public class CrearVariables {
+
+	public static void main(String[] args) {
+
+		
+		// Declara e inicializa tus variables aquí.
+		// La edad de un estudiante
+		// El curso en el que está un estudiante
+		// El promedio (GPA) de un estudiante
+		// La puntuación de un juego
+		// Si quieres continuar el juego o no
+		// La inicial del segundo nombre
+		// El color de un bloque
+		// Si una persona está casada o no
+		// El precio de un artículo del menú
+		// El número de grados para girar un robot hacia la derecha
+		// La fecha del primer día de clases
+		// El número de minutos hasta que tu pedido esté listo
+		// El nombre de la escuela
+		// El precio por unidad de una fruta
+		// Tu dirección de la calle
+		// El número de teléfono de un cliente
+		// Una caída de meteorito que incluya el nombre del meteorito, la fecha en que cayó, su masa y el lugar de impacto
+		// Una carta de una baraja de 52 cartas
+		// La respuesta de un invitado indicando si asistirá o no a una fiesta
+
+		
+	}
+
+}
