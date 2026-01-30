a65110ff7736f08e0d66573152c4b9f041bd51d4
Luis Martínez Redondo <lmartinezr10@educarex.es>
2026-01-30 08:35:17 +0100

    Datos ejemplo vehículos

---
+++ src/unidad5/ejemplos/vehiculos/datos/Datos.java
@@ -0,0 +1,29 @@
+package unidad5.ejemplos.vehiculos.datos;
+
+public class Datos {
+	
+	public static String[] datosVehiculos = {
+		    "1;COCHE;1234ABC;Toyota;Corolla;5",
+		    "2;MOTO;2345BCD;Yamaha;MT-07;689",
+		    "3;CAMION;3456CDE;Volvo;FH16;18000",
+		    "4;ELECTRICO;4567DEF;Tesla;Model 3;491",
+		    "5;COCHE;5678EFG;Seat;Ibiza;3",
+		    "6;MOTO;6789FGH;Honda;CB500F;471",
+		    "7;CAMION;7890GHI;MAN;TGX;26000",
+		    "8;ELECTRICO;8901HIJ;Nissan;Leaf;385",
+		    "9;COCHE;9012IJK;Ford;Focus;5",
+		    "10;MOTO;0123JKL;Kawasaki;Z650;649",
+		    "11;COCHE;1122KLM;Volkswagen;Golf;5",
+		    "12;CAMION;2233LMN;Scania;R500;24000",
+		    "13;ELECTRICO;3344MNO;Hyundai;Kona;484",
+		    "14;MOTO;4455NOP;BMW;R1250GS;1254",
+		    "15;COCHE;5566OPQ;Renault;Clio;3",
+		    "16;CAMION;6677PQR;Iveco;Stralis;20000",
+		    "17;ELECTRICO;7788QRS;Kia;EV6;528",
+		    "18;COCHE;8899RST;Peugeot;308;5",
+		    "19;MOTO;9900STU;Suzuki;SV650;645",
+		    "20;COCHE;1011TUV;Audi;A3;5"
+		};
+
+
+}
