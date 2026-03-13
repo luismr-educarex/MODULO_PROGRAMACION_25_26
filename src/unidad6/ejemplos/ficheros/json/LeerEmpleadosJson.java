8550b4ffcdd61ca87f5c8d4e9e521a96bc0cd580
Luis Martínez <lmartinezr10@educarex.es>
2026-03-13 11:52:40 +0100

    Ejemplo leer JSON

---
+++ src/unidad6/ejemplos/ficheros/json/LeerEmpleadosJson.java
@@ -0,0 +1,47 @@
+package unidad6.ejemplos.ficheros.json;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LeerEmpleadosJson {
+
+	public static void main(String[] args) {
+	
+		Gson gson = new Gson();
+		Type empleadoLista =
+				new TypeToken<List<Empleado>>(){}.getType();
+				
+		try(FileReader lector = new FileReader("empleados.json")){
+			
+			List<Empleado> empleados = 
+					       gson.fromJson(lector,empleadoLista);
+			
+			for(Empleado empleado:empleados) {
+				System.out.println(empleado);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
